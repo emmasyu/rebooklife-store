c83ea87626957b0c8ce3ebbba46fed5f50f7e393
EmmaSyu <emma831215@gmail.com>
2023-05-30 16:33:16 +0800

    feat: 🎸 add order copy id and push to pay function button

--- src/components/front/OrderInfo.vue
+++ src/components/front/OrderInfo.vue
@@ -15,6 +15,12 @@
       <div class="d-flex mb-4 mb-lg-7 gap-4">
         <p class="min-w-content">訂單編號</p>
         <p class="fw-normal">{{ $route.params.orderId }}</p>
+        <button
+          class="btn btn-outline-primary btn-sm py-1"
+          @click.prevent="copyId"
+        >
+          複製
+        </button>
       </div>
       <div class="d-flex mb-4 mb-lg-7 gap-4">
         <p class="min-w-content">收件人名</p>
@@ -38,11 +44,22 @@
         <p class="min-w-content">訂單備註</p>
         <p class="fw-normal">{{ order.message }}</p>
       </div>
-      <div v-if="order.paid_date" class="d-flex mb-4 mb-lg-7 gap-4">
+      <div class="d-flex mb-4 mb-lg-7 gap-4 hstack">
         <p class="min-w-content">付款日期</p>
         <p class="fw-normal">
-          {{ $filters.convertTimeToLocale(order.paid_date) }}
+          {{
+            order.paid_date
+              ? $filters.convertTimeToLocale(order.paid_date)
+              : "尚未付款"
+          }}
         </p>
+        <button
+          class="btn btn-outline-primary btn-sm py-1"
+          v-if="!order.paid_date && $route.name !== 'orderPay'"
+          @click.prevent="pushToPay()"
+        >
+          前往付款
+        </button>
       </div>
       <div class="d-flex gap-4">
         <p class="min-w-content">訂單金額</p>
@@ -63,6 +80,8 @@
 </template>
 <script>
 import OrderList from "@/components/front/OrderList.vue";
+import { mapActions } from "pinia";
+import useStateStore from "@/stores/states.js";
 
 export default {
   components: { OrderList },
@@ -73,8 +92,22 @@ export default {
     };
   },
   methods: {
+    ...mapActions(useStateStore, ["pushToastMessage"]),
     toggleList() {
       this.isOpenList = !this.isOpenList;
+    },
+    pushToPay() {
+      this.$router.push(
+        `/bookstore/cart/order_pay/${this.$route.params.orderId}`
+      );
+    },
+    async copyId() {
+      try {
+        await navigator.clipboard.writeText(this.$route.params.orderId);
+        this.pushToastMessage(`已成功複製 訂單編號 到剪貼簿`);
+      } catch (err) {
+        console.log(err);
+      }
     },
   },
 };
