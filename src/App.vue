b735d327ad19ff94261d6f1bb7702041872fe76d
EmmaSyu <emma831215@gmail.com>
2023-06-01 16:29:37 +0800

    feat: 🎸 add aos animation

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,16 @@
 import { RouterLink, RouterView } from "vue-router";
 import LoadingOverlay from "@/components/global/LoadingOverlay.vue";
 import Toasts from "@/components/global/Toasts.vue";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 export default {
   components: {
     LoadingOverlay,
     Toasts,
+  },
+  mounted() {
+    AOS.init();
   },
 };
 </script>
--- src/views/front/Store/Bookstore/BookStoreView.vue
+++ src/views/front/Store/Bookstore/BookStoreView.vue
@@ -2,21 +2,43 @@
   <div class="store-bg w-100">
     <div class="container fs-6 fs-lg-5 fs-xl-4">
       <div
-        class="vh-100 d-flex justify-content-center align-items-center position-relative justify-content-lg-between"
+        class="vh-100 vstack justify-content-center align-items-center position-relative"
       >
         <div
-          class="text-secondary fs-3-first-letter fs-lg-1-first-letter fw-bold ps-lg-12"
+          class="text-secondary fs-3-first-letter fs-lg-1-first-letter fw-bold pt-12"
         >
-          <p class="mb-3">當這本書再次找到它的主人</p>
-          <p class="mb-3">
-            將賦予它一段新的生命<span class="d-none d-lg-inline-block"
+          <p
+            class="mb-3"
+            data-aos="fade-up-right"
+            data-aos-delay="300"
+            data-aos-duration="1500"
+          >
+            當這本書再次找到它的主人
+          </p>
+          <p
+            class="mb-3"
+            data-aos="fade-up-left"
+            data-aos-delay="600"
+            data-aos-duration="1500"
+          >
+            將賦予它一段新的生命
+            <span class="d-none d-lg-inline-block"
               >，為彼此開啟一段新的知識旅程...</span
             >
           </p>
-          <p class="d-lg-none">為彼此開啟一段新的知識旅程...</p>
+          <p
+            class="d-lg-none"
+            data-aos="fade-up-right"
+            data-aos-delay="900"
+            data-aos-duration="1500"
+          >
+            為彼此開啟一段新的知識旅程...
+          </p>
         </div>
-        <div class="position-absolute bottom-0 pb-5 float-vertical d-lg-none">
-          <p class="text-primary">看書去</p>
+        <div
+          class="position-absolute position-lg-static bottom-0 pb-11 pb-lg-0 ps-lg-5 pt-lg-10 float-vertical"
+        >
+          <p class="text-primary d-lg-none">看書去</p>
           <img src="@/assets/images/guide-line.png" alt="go to read" />
         </div>
       </div>
