be900e6d25dad9b73ff76c3976eea2ebfa49f291
EmmaSyu <emma831215@gmail.com>
2023-07-26 00:06:41 +0800

    feat: 🎸 add validator, remove irregular whitespace

--- src/views/front/Store/components/Footer.vue
+++ src/views/front/Store/components/Footer.vue
@@ -17,20 +17,34 @@
           >
             訂閱二搜書最新活動訊息
           </h4>
-          <div class="w-100 d-flex pb-9">
-            <input
-              type="text"
-              placeholder="請輸入 Email 接收最新活動訊息"
-              class="w-100 text-center border-0 rounded-0 bg-transparent border-bottom border-secondary outline-none text-lg-start"
-            />
-            <a
-              href="#"
-              class="w-3 px-2 w-lg-6 px-lg-4 h-3 h-lg-6 d-flex justify-content-center align-items-center bg-secondary bg-secondary-light-hover text-light fs-5 fs-lg-3"
-              @click.prevent
-            >
-              <font-awesome-icon :icon="['fas', 'envelope']" />
-            </a>
-          </div>
+          <VForm class="pb-9" v-slot="{ errors }" @submit="sendEmail">
+            <ErrorMessage
+              name="電子郵件"
+              class="invalid-feedback ps-4 h-3"
+              :class="{ 'd-block': errors['電子郵件'] }"
+            ></ErrorMessage>
+            <div class="w-100 d-flex">
+              <VField
+                type="email"
+                placeholder="請輸入 Email 接收最新活動訊息"
+                class="w-100 text-center border-0 rounded-0 border-bottom border-secondary outline-none text-lg-start"
+                name="電子郵件"
+                :class="[
+                  errors['電子郵件']
+                    ? 'border-danger is-invalid bg-danger bg-opacity-25'
+                    : 'border-secondary bg-transparent',
+                ]"
+                rules="email"
+                v-model="email"
+              />
+              <button
+                type="submit"
+                class="w-3 px-2 w-lg-6 px-lg-4 h-3 h-lg-6 d-flex justify-content-center align-items-center bg-secondary bg-secondary-light-hover text-light fs-5 fs-lg-3 border-0"
+              >
+                <font-awesome-icon :icon="['fas', 'envelope']" />
+              </button>
+            </div>
+          </VForm>
         </div>
         <div
           class="col-lg-7 d-flex flex-column align-items-center justify-content-between flex-lg-row"
@@ -44,12 +58,12 @@
             <RouterLink
               class="text-decoration-underline-hover text-secondary mb-4"
               to="/#faq"
-              >常見問題　 | FAQ</RouterLink
+              >常見問題 | FAQ</RouterLink
             >
             <RouterLink
               class="text-decoration-underline-hover text-secondary"
               to="/#contact"
-              >聯絡我們　 | Contct Us</RouterLink
+              >聯絡我們 | Contct Us</RouterLink
             >
           </div>
 
@@ -57,9 +71,9 @@
             class="row pb-4 text-center fs-3 align-self-lg-end fs-lg-2 pe-xl-8 pe-xxl-11"
           >
             <a
-              href="#"
+              href="https://github.com/EmmaSyu"
               class="col text-secondary text-secondary-light-hover"
-              @click.prevent
+              target="_blank"
               ><font-awesome-icon :icon="['fab', 'square-github']"
             /></a>
             <a
@@ -90,6 +104,33 @@
     </div>
   </footer>
 </template>
+
+<script>
+import { mapState, mapActions } from "pinia";
+import useStatesStore from "@/stores/states.js";
+
+export default {
+  data() {
+    return {
+      email: "",
+    };
+  },
+  computed: {
+    ...mapState(useStatesStore, ["toastMessage"]),
+  },
+  methods: {
+    ...mapActions(useStatesStore, ["pushToastMessage"]),
+    sendEmail() {
+      if (this.email === "") {
+        this.pushToastMessage("欄位不可為空", { success: false });
+      } else {
+        this.pushToastMessage("郵件訂閱傳送成功");
+        this.email = "";
+      }
+    },
+  },
+};
+</script>
 
 <style scoped>
 footer {
