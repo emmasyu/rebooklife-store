56ce5e9df59c640775390d81993ce96d6154ff50
EmmaSyu <emma831215@gmail.com>
2023-07-25 23:53:29 +0800

    feat: 🎸 add hint to go store page

--- src/views/front/Home/HomeView.vue
+++ src/views/front/Home/HomeView.vue
@@ -3,12 +3,14 @@
   <AdCouponCol />
   <Navbar />
   <Content />
+  <Hint />
 </template>
 
 <script>
 import Navbar from "./components/Navbar.vue";
 import Content from "./components/Content.vue";
 import Background from "./components/Background.vue";
+import Hint from "./components/Hint.vue";
 import AdCouponCol from "@/components/front/AdCouponCol.vue";
 
 export default {
@@ -16,6 +18,7 @@ export default {
     Navbar,
     Content,
     Background,
+    Hint,
     AdCouponCol,
   },
 };
---
+++ src/views/front/Home/components/Enter.vue
@@ -0,0 +1,56 @@
+<template>
+  <div
+    class="transition position-fixed ps-3 py-3 mt-10 pe-4 top-0 w-5 text-gray fs-6 bg-black bg-opacity-75 rounded-start-3 start-100"
+    :class="{ 'show-hint': showHint }"
+  >
+    <button
+      type="button"
+      class="text-white bg-transparent border-0 d-block p-0 text-secondary-hover"
+      @click="showHint = false"
+    >
+      ×
+    </button>
+    <span class="d-lg-none">≡</span>提示｜<RouterLink
+      to="/bookstore"
+      class="text-white text-secondary-hover"
+      >找書 ↓ 前往商城購物</RouterLink
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showHint: false,
+    };
+  },
+  methods: {
+    enterStore() {
+      this.$router.push("/bookstore");
+    },
+  },
+  mounted() {
+    setTimeout(() => {
+      this.showHint = true;
+    }, 3000);
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.show-hint {
+  transform: translateX(-100%);
+}
+.float-vertical {
+  animation: float-vertical 0.5s ease-in-out infinite alternate;
+}
+@keyframes float-vertical {
+  from {
+    transform: translateY(0rem);
+  }
+  to {
+    transform: translateY(-0.5rem);
+  }
+}
+</style>
